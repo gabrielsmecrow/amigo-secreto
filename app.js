d29810b5b6e6f9433f2848561c9c3804ed467f3e
Gabs <smecrowl9@gmail.com>
2025-07-26 18:47:05 -0300

    trocando cores no css e implementando o input do usuário no javascript

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function adicionarAmigo() {
+  let amigo = document.getElementById("amigo").value;
+
+  if (amigo == null) {
+    alert("Por favor, insira um nome.");
+  } else {
+    nomes.push(amigo);
+    adicionandoNaLista();
+    limparTela();
+  }
+  console.log(nomes);
+}
+
+function limparTela() {
+  let amigo = document.getElementById("amigo");
+  amigo.value = "";
+}
+
+function adicionandoNaLista() {
+  let li = document.getElementById("listaAmigos");
+  li.innerHTML = "";
+  for (i = 0; i < nomes.length; i++) {
+    li.innerHTML += `${nomes[i]}<br>`;
+  }
+}
